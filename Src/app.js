0ec3ff0242016a6dc6806912ce1252327d94bc6c
angemas <angelique.massolo@outlook.com>
2022-03-31 15:30:44 +0800

    add search engine

--- Src/app.js
+++ Src/app.js
@@ -1,3 +1,27 @@
+function formatdate(timestamp) {
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let date = new Date(timestamp);
+  let day = date.getDay();
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let min = date.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let fulldate = `${weekdays[day]}, ${hour}:${min}`;
+  return fulldate;
+}
+
 function showTemp(response) {
   let city = document.querySelector("#current-city");
   let currentTemp = document.querySelector("#current-temp");
@@ -5,6 +29,7 @@ function showTemp(response) {
   let feelsLike = document.querySelector("#feels-like");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
+  let date = document.querySelector("#lastdate");
 
   city.innerHTML = response.data.name;
   currentTemp.innerHTML = Math.round(response.data.main.temp);
@@ -15,11 +40,26 @@ function showTemp(response) {
   iconindex = response.data.weather[0].icon;
   iconurl = `http://openweathermap.org/img/wn/${iconindex}@4x.png`;
   document.getElementById("current-emoji").src = iconurl;
+  document.getElementById("current-emoji").alt =
+    response.data.weather[0].description;
+  date.innerHTML = formatdate(response.data.dt * 1000);
 }
 
-let apiKey = "c0ed04c902a245721bb289e92dca75fe";
-let city = "Singapore";
-let units = "metric";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function search(city) {
+  let apiKey = "c0ed04c902a245721bb289e92dca75fe";
+  let units = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-axios.get(url).then(showTemp);
+  axios.get(url).then(showTemp);
+}
+
+function usersearch(event) {
+  event.preventDefault();
+  let usercity = document.querySelector("#user-city").value;
+  search(usercity);
+}
+
+search("Singapore");
+
+let submit = document.querySelector("#search-city");
+submit.addEventListener("submit", usersearch);
